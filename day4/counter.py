e98cd379ded0669aa591db5e8a8553d2b58fc23e
Thea Meimoun <158083278+theammn@users.noreply.github.com>
2024-05-13 10:20:35 +0300

    Create counter.py

---
+++ day4/counter.py
@@ -0,0 +1,3 @@
+#How many characters does the file have.
+#How many lines are in the file.
+#How many words are in the file. (For our purpose you can assume that any two things separated by spaces are separate words.
