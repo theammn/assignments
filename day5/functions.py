cdf39f034587ab79a2037acc522efe090d00bc2f
Thea Meimoun <158083278+theammn@users.noreply.github.com>
2024-05-21 21:04:54 +0300

    Update functions.py

--- day5/functions.py
+++ day5/functions.py
@@ -6,3 +6,16 @@ def count_words(lines):
 
 def count_lines(lines):
     return len(lines)
+
+def file_statistics(filename):
+    try:
+        with open(filename, 'r') as file:
+            num_characters = num_lines = num_words = 0
+            for line in file:
+                num_lines += 1
+                num_characters += len(line)
+                num_words += len(line.split())
+        return num_characters, num_lines, num_words
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return -1, -1, -1
