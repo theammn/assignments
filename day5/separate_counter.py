1a3ff65fe4bb03d30615819dd14c0908621fb93f
Thea Meimoun <158083278+theammn@users.noreply.github.com>
2024-05-21 20:55:22 +0300

    Update separate_counter.py

--- day5/separate_counter.py
+++ day5/separate_counter.py
@@ -1,1 +1,13 @@
+# file_utils.py
+def file_statistics(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            num_characters = count_characters(lines)
+            num_lines = count_lines(lines)
+            num_words = count_words(lines)
+        return num_characters, num_lines, num_words
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return -1, -1, -1
 
