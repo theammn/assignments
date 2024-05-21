5389eca9914e215e91c0415ae0897336bdc15aee
Thea Meimoun <158083278+theammn@users.noreply.github.com>
2024-05-21 20:54:16 +0300

    Update functions.py

--- day5/functions.py
+++ day5/functions.py
@@ -1,13 +1,8 @@
-def functions(text):
-    try:
-        with open(text, 'r') as file:
-            num_characters = num_lines = num_words = 0
-            for line in file:
-                num_lines += 1
-                num_characters += len(line)
-                num_words += len(line.split())
-        return num_characters, num_lines, num_words
-    except FileNotFoundError:
-        print(f"File not found: {text}")
-        return -1, -1, -1
+def count_characters(lines):
+    return sum(len(line) for line in lines)
 
+def count_words(lines):
+    return sum(len(line.split()) for line in lines)
+
+def count_lines(lines):
+    return len(lines)
